977f28171221f32f76bacb4f3efab6b8d596ad05
Emanuel Lima <spryler@hotmail.com>
2022-12-30 13:55:56 -0300

    start

--- src/axios-api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const getAPI = axios.create({
-  baseURL: "https://pokeapi.co/api/v2/",
-  //baseURL: "http://localhost:8000/api/",
-  timeout: 60000,
-});
-
-export { getAPI };
--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
-
-  /*  devServer: {
-    host: "192.168.0.12"
-  }*/ transpileDependencies: [
-    "vuetify"
-  ]
-};
