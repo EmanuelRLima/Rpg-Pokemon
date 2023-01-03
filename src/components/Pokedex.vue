7a9d2a1dbbbc3f304fbf056cc28f912ede209ef5
Emanuel Lima <spryler@hotmail.com>
2023-01-03 14:18:37 -0300

    updated

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -3,32 +3,64 @@
     <div class="d-flex justify-content-center">
 
       <div class="position_layout_grid">
+        <div class="nav_layout">
+          <button class="Button_pokedex"></button> 
+          <button class="Button_pokedex_mini first"></button> 
+          <button class="Button_pokedex_mini second"></button> 
+          <button class="Button_pokedex_mini third"></button>  
+        </div>
         <div class="screen_layout_grid">
-          <div class="screen_poke background_campo"></div>
+          <div class="screen_poke background_scren">
+            <img style="width: 100%; height: 100%; object-fit: contain;" :src="pokemon_img">
+          </div>
         </div>
         <div class="left_nav_layout_grid"></div>
+        <div class="footer_layout_grid">
+
+          <div class="search_poke_grid">
+              <button style="width: 35px;height: 50px;border-radius: 4px;border: 1px solid rgba(0, 0, 0, 0.38);" @click="Get_Pokemon();"><i class="fa fa-search"></i></button>
+              <InputText v-model="pokemon" class="search_poke" placeholder="pokemon"/>
+          </div>
+
+          <div class="joystic">
+            <button class="joystic_up"><i class="fa fa-arrow-up"></i></button>
+            <button class="joystic_left"><i class="fa fa-arrow-up"></i></button>
+            <button class="joystic_down"><i class="fa fa-arrow-up"></i></button>
+            <button class="joystic_right"><i class="fa fa-arrow-up"></i></button>
+          </div>
+        </div>
       </div>
     </div>
 </div> 
 </template>
 <script>
 
-//import { AxiosAPI } from "@/axios";
+import { AxiosAPI } from "@/axios";
 import "../assets/css/pokedex.css"
+import img_wait from "../assets/img/Aguardando.png"
 export default {
   name: "App",
   components: {
   },  
   data() {
     return {
-
+      pokemon: "",
+      pokemon_img: img_wait,
     };
   },
 
   mounted() {
   },
+
   methods: {
- 
+    Get_Pokemon(){
+      AxiosAPI.get("pokemon/" + this.pokemon.toLowerCase())
+      .then((resp) =>{
+        console.log(resp)
+        this.pokemon_img = resp.data.sprites.other.home.front_default
+      })
+    },
+   
   },
 };
 
